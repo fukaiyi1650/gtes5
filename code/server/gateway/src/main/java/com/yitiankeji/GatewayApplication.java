7803c03a43a36bc8a17f51d6467196ac2863dcb3
tianlihu <35980609+tianlihu@users.noreply.github.com>
2020-07-24 15:25:29 +0800

    恢复错误的提交

--- code/server/gateway/src/main/java/com/yitiankeji/GatewayApplication.java
+++ code/server/gateway/src/main/java/com/yitiankeji/GatewayApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
